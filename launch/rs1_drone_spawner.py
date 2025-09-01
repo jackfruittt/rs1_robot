0ca558fa6c6c9a507376eda8a092f4297ab75bb0
ace-rv <aceviray5@gmail.com>
2025-09-01 18:24:16 +1000

    Edited drone spawn location to be ontop of world_1 helipad

--- launch/rs1_drone_spawner.py
+++ launch/rs1_drone_spawner.py
@@ -55,10 +55,10 @@ def spawn_multiple_drones(context, *args, **kwargs):
     for i in range(1, num_drones + 1):
         drone_name = f'rs1_drone_{i}'
         
-        # Position drones in a line (same spacing as before)
-        x_pos = (i - 1) * 3.0  # Start at 0, then 3, 6, 9...
-        y_pos = 0.0
-        z_pos = 1.0
+        # Position drones in a line (same spacing as before) - Edited to spawn on World 1 Helipad Center
+        x_pos = (i - 10.43)  * 3.0  # Start at 0, then 3, 6, 9...
+        y_pos = 14.0
+        z_pos = 13.1
         
         print(f"Creating drone {i}: {drone_name} at ({x_pos}, {y_pos}, {z_pos})")
         
